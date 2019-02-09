4f806ffed279e5618dc85e7ea0deb78e3c1fb6c8
Andrei <l2.ls@yandex.ru>
2019-02-09 23:02:52 +0300

    Инициализация БД

---
+++ schema.sql
@@ -0,0 +1,47 @@
+CREATE DATABASE yeticave
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+
+USE yeticave;
+
+CREATE TABLE categories (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name CHAR(50)
+);
+
+CREATE TABLE products (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT,
+  category_id INT,
+  date_create TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  date_end TIMESTAMP,
+  name CHAR(100),
+  description CHAR(255),
+  img_url CHAR(100),
+  price INT,
+  price_step INT
+);
+
+CREATE INDEX p_name ON products(name);
+
+CREATE TABLE rates (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT,
+  product_id INT,
+  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  sum INT
+);
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  product_id INT,
+  rate_id INT,
+  reg_date TIMESTAMP,
+  email CHAR(100),
+  name CHAR(50),
+  password CHAR(50),
+  avatar_url CHAR(100),
+  contacts CHAR(255)
+);
+
+CREATE UNIQUE INDEX email ON users(email);
