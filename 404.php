44ccd7783ca2f341c99ecbee297dcae537adddf8
Andrei <l2.ls@yandex.ru>
2019-02-23 13:47:26 +0300

    Отображение запрашиваемого лота

--- 404.php
+++ 404.php
@@ -1,28 +1,18 @@
 <?php
 date_default_timezone_set('Europe/Moscow');
 
-$isAuth = rand(0, 1);
+require('data.php');
+require('functions.php');
 $config = include('config/config.php');
 
+$isAuth = rand(0, 1);
 $link = mysqli_connect($config['db']['host'], $config['db']['user'], $config['db']['pass'], $config['db']['name']);
 
 $categoriesSql = 'SELECT name FROM categories';
-
-$productsSql = 'SELECT p.id as id, p.name as title, price, img_url as url, c.name as category, DATE(date_end) as date_end
-FROM products p
-JOIN categories c ON p.category_id = c.id
-ORDER BY p.id DESC';
-
-require('data.php');
-require('functions.php');
-
 $categories = getDataAsArray($link, $categoriesSql);
-$products = getDataAsArray($link, $productsSql);
 
-$pageContent = includeTemplate('index.php', [
-    'products' => $products,
-    'categories' => $categories,
-    'config' => $config
+$pageContent = includeTemplate('404.php', [
+    'categories' => $categories
 ]);
 
 $layoutContent = includeTemplate('layout.php', [
@@ -34,3 +24,4 @@ $layoutContent = includeTemplate('layout.php', [
 ]);
 
 print($layoutContent);
+
